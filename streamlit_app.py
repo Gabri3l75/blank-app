9905f3d2302aee9a0edafa6c1b139564cf561136
Gabri3l75 <160071172+Gabri3l75@users.noreply.github.com>
2025-02-03 21:10:42 +0000

    Actualización de la funcionalidad X y corrección de error Y

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🎈 Mi nueva app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Vamos a iniciar a construir! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 cantidad = st.slider('Selecciona la cantidad')
 st.write(f'La cantidad seleccionada es: {cantidad}')
