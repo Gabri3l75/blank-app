6775ebaab39415e7e2644b7cb74de888c70b6ac2
Gabri3l75 <160071172+Gabri3l75@users.noreply.github.com>
2025-02-03 21:14:30 +0000

    Actualización todo OK

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🎈 Mi nueva app")
 st.write(
-    "Vamos a iniciar a construir! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Ok Nueva 2 Vamos a iniciar a construir! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 cantidad = st.slider('Selecciona la cantidad')
 st.write(f'La cantidad seleccionada es: {cantidad}')
